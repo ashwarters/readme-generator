e1698d8dd49b91c7dc041d0f2eab1392aeb87691
Ashley Warters <ashwarters@gmail.com>
2020-10-04 15:32:26 -0400

    updated questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,58 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
 // array of questions for user
 const questions = [
+    {
+        type:'input',
+        name:'title',
+        message: `List your project's title:`
+    },
+    {
+        type:'input',
+        name:'badge-link',
+        message:'List desired badge links:'
+    },
+    {
+        type:'input',
+        name:'project-description',
+        message: 'Describe your project:'
+    },
+    {
+        type:'input',
+        name:'install',
+        message:'List installation instructions:'
+    },
+    {
+        type:'input',
+        name:'usage',
+        message:`Describe the project's usage:`
+    },
+    {
+        type:'input',
+        name:'licensing',
+        message:`List the licensing for the project:`
+    },
+    {
+        type:'input',
+        name:'contributors',
+        message:'List members who contributed to the project:'
+    },
+    {
+        type:'input',
+        name:'test',
+        message:'Provide project tests:'
+    },
+    {
+        type:'input',
+        name:'userInfo',
+        message:`What's your Github username?`
+    },
+    {
+        type:'input',
+        name:'repoLink',
+        message:`Insert your repository link:`
+    }
 
 ];
 
